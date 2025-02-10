7ad9342750968616e879d3ad667d730f2950928e
Advait Pandharpurkar <38832648+lordofwizard@users.noreply.github.com>
2025-02-10 11:55:16 +0000

    Added tests for adding blank commits

--- src/git_tests.rs
+++ src/git_tests.rs
@@ -105,4 +105,57 @@ mod tests {
         assert!(test_dir.join("commit_maker.txt").exists());
         fs::remove_dir_all(&test_dir).unwrap();
     }
+
+
+
+}
+
+#[cfg(test)]
+mod tests_for_commit {
+    use crate::git::{add_commits_in_date_range, setup_git_folder};
+
+    use super::*;
+    use std::process::Command;
+    use std::fs;
+    use chrono::NaiveDate;
+
+    #[test]
+    fn test_correct_number_of_commits() {
+        let repo_path = setup_git_repo();
+        let repo_str = repo_path.as_str();
+        setup_git_folder(repo_str);
+        
+        add_commits_in_date_range(repo_str, "01-02-2024", "05-02-2024");
+        
+        let output = Command::new("git")
+            .args(["rev-list", "--count", "HEAD"])
+            .current_dir(repo_str)
+            .output()
+            .expect("Failed to count commits");
+        
+        let commit_count = String::from_utf8(output.stdout).unwrap().trim().parse::<usize>().unwrap();
+        assert_eq!(commit_count, 6, "Expected 6 commits, found {}, (Initial Commit + Blank Commits", commit_count);
+    }
+    
+    #[test]
+    #[should_panic(expected = "Invalid start date format")]
+    fn test_invalid_date_format() {
+        let repo_path = env::temp_dir().join("test_git_repo_invalid");
+        let repo_str = repo_path.to_str().unwrap();
+        fs::create_dir_all(repo_str).unwrap();
+        Command::new("git").arg("init").current_dir(repo_str).output().unwrap();
+        
+        add_commits_in_date_range(repo_str, "32-01-2021", "05-02-2021");
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid start date format")]
+    fn test_invalid_date_format_2() {
+        let repo_path = env::temp_dir().join("test_git_repo_invalid_2");
+        let repo_str = repo_path.to_str().unwrap();
+        fs::create_dir_all(repo_str).unwrap();
+        Command::new("git").arg("init").current_dir(repo_str).output().unwrap();
+        
+        add_commits_in_date_range(repo_str, "29-02-2021", "05-02-2021");
+    }
 }
